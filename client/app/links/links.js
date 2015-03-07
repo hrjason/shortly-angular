993805e588ab8b0d807ad56fb9cb6d0d3c7f302d
jasonjchang <hrjasonchang@gmail.com>
2015-03-06 17:06:13 -0800

    Link Controller

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,52 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
-});
+.controller('LinksController',['$scope','Links', function ($scope, Links) {
+  // $scope.link = [
+  //   // {
+  //     // title: 'buttfaces',
+  //     // visits: '69',
+  //     // url: 'http://buttfaces.com',
+  //     // base_url: 'localface.com/facepoop',
+  //     // code: '42+69'
+  //   // }
+  // ];
+  $scope.data = {
+    links: null
+  };
+
+  // $scope.getLinks = function () {
+  //   Links.getLinks()
+  //     .then(function (link) {
+  //       $scope.links.push(link);
+  //       // $window.localStorage.setItem('com.shortly', link);
+  //       // $location.path('/links');
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // };
+
+
+  $scope.getLinks = function() {
+    // console.log(Links);
+    Links.getLinks()
+      .then(function (links) {
+        console.log(links);
+        $scope.data.links = links;
+        // $window.localStorage.setItem('com.shortly', link);
+        // $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.getLinks();
+
+  // ask service to give us all the deets
+    // service will ask databasu for le infos
+    // get all bak; force jason chang to stop saying doggie
+  // get info back from service
+    // push into links array
+
+}]);
